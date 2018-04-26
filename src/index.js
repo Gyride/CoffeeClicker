11e3cd128c285f6f5c1fa685534b293d053cd47e
Gyride <sebast.jar@gmail.com>
2018-04-26 18:50:50 -0400

    import directory of Main component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './stylesheets/index.css';
-import Main from './Main';
+import Main from './components/Main';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<Main />, document.getElementById('root'));
